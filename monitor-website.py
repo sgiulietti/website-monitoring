e0affa3bf624e9e0197f42afd076430b7e9bee5d
Sergio <sergiog.giulietti@gmail.com>
2024-03-22 01:45:30 -0300

    update monitoring python file

--- monitor-website.py
+++ monitor-website.py
@@ -17,6 +17,29 @@ def send_notification(email_msg):
         message = f"Subject: app not available\n{email_msg}"
         smtp.sendmail(EMAIL_ADDRESS,EMAIL_ADDRESS,message)
 
+#function to restart the app
+def restart_container():
+    ssh = paramiko.SSHClient()
+    ssh.net_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(hostname=xxx, username=xxx, key_filename=xxx)
+    stdin, stdout, stderr = ssh.exec_command('docker start containerid')
+    print(stdout.readlines())
+    ssh.close()
+
+# Get the droplet_id from the server name
+def get_droplet_info(droplet_name):
+    api_url = f'https://api.digitalocean.com/v2/droplets?name={droplet_name}'  #Url that will be used to call API request
+    response = requests.get(api_url, headers=headers) #Get the response from API after sending request
+    if response.status_code == 200: #If response is success, show the response. Otherwise return nothing
+        data = (json.loads(response.content.decode('utf-8')))
+        droplet_id = data['droplets'][0]['id']
+        return droplet_id
+    
+def reboot_droplet(droplet_id):
+    api_droplet_action_url = f'https://api.digitalocean.com/v2/droplets/{droplet_id}/actions' #Url that will be used for API request
+    action = {"type":"reboot"} #Gathering necessary information
+    requests.post(api_droplet_action_url, headers=headers, json=action) #Sending the gathered information with post request to reboot the droplet
+
 try:
     response = requests.get('http://104.236.6.212:8080/')
     if response.status_code == 200:
@@ -27,12 +50,8 @@ try:
         send_notification(msg)
 
         #restart the app
-        ssh = paramiko.SSHClient()
-        ssh.net_missing_host_key_policy(paramiko.AutoAddPolicy())
-        ssh.connect(hostname=xxx, username=xxx, key_filename=xxx)
-        stdin, stdout, stderr = ssh.exec_command('docker start containerid')
-        print(stdout.readlines())
-        ssh.close()
+        restart_container()
+        
 except Exception as ex:
     print(f'Connection error: {ex}')
     msg = 'App not accessible'
@@ -43,23 +62,10 @@ except Exception as ex:
     headers = {'Content-Type': 'application/json', 'Authorization': f'Bearer {api_token}'}  # Header information that will be sent to the digitalocean in order to authenticate user
     droplet_name = 'test-droplet'
 
-    # Get the droplet_id from the server name
-    def get_droplet_info(droplet_name):
-        api_url = f'https://api.digitalocean.com/v2/droplets?name={droplet_name}'  #Url that will be used to call API request
-        response = requests.get(api_url, headers=headers) #Get the response from API after sending request
-        if response.status_code == 200: #If response is success, show the response. Otherwise return nothing
-            data = (json.loads(response.content.decode('utf-8')))
-            droplet_id = data['droplets'][0]['id']
-            return droplet_id
-    
     droplet_id = get_droplet_info(droplet_name)
 
     # Reboot the server
-    def reboot_droplet(droplet_id):
-        api_droplet_action_url = f'https://api.digitalocean.com/v2/droplets/{droplet_id}/actions' #Url that will be used for API request
-        action = {"type":"reboot"} #Gathering necessary information
-        requests.post(api_droplet_action_url, headers=headers, json=action) #Sending the gathered information with post request to reboot the droplet 
-    
     reboot_droplet(droplet_id)
 
-    # restart application       
+    # restart application
+    restart_container()   
